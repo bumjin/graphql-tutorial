542eda2163f2b097653812d94a910c15c1f49341
Beomjin Park <bumjin@gmail.com>
2018-01-06 11:52:10 +0900

    add CRUD

--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,25 @@
+import mongoose from 'mongoose';
+import authorModel from './models/author.js';
 const resolvers = {
     Query: {
-        authors: () => {
-            return authors
+        authors: (root, {age}) => {
+            return authorModel.find({age:age})
         },
         author: (root, {id}) => {
-            return authors.find(author => author.id === id);
+            return authorModel.findOne({id: id});
+        }
+    }, 
+    Mutation: {
+        addAuthor: (root, {name, age, books}) => {
+            const author = new authorModel({age: age, name: name, books: books});
+            return author.save();
+        },
+        deleteAuthor: (root, {id}) => {
+            //return authorModel.remove({id: id})
+            return authorModel.findOneAndRemove({id: id});
+        },
+        updateAuthor: (root, {id, name}) => {
+            return authorModel.findOneAndUpdate({id: id}, {name: name});
         }
     }
 }
